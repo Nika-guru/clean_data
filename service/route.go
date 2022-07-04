56ea5693a69ae0ed1152bdeb5fedf2035b427194
minhdan <nguyenmd.works@gmail.com>
2022-07-04 15:04:31 +0700

    done base

--- service/route.go
+++ service/route.go
@@ -1,13 +1,21 @@
 package service
 
 import (
+	"base/pkg/auth"
 	"base/pkg/router"
-	// "base/service/index"
+	"base/service/index"
 	"base/service/users/controller"
 )
 
 // LoadRoutes to Load Routes to Router
 func LoadRoutes() {
+
+	// Set Endpoint for Authorization Functions
+	router.Router.With(auth.Basic).Get(router.RouterBasePath+"/auth", index.GetAuth)
+
+	// Set Endpoint for admin
+	router.Router.Get(router.RouterBasePath+"/", index.GetIndex)
+	router.Router.Get(router.RouterBasePath+"/health", index.GetHealth)
 
 	// Set Endpoint for User Functions
 	router.Router.Get(router.RouterBasePath+"/users", controller.GetUser)
